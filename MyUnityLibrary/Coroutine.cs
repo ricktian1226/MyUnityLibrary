9a8879d392e587f75c4e6e29929cf739b1fa728d
田贵茂 <tiangm@feiyu.com>
2016-06-02 16:15:20 +0800

    +提交代码

---
+++ MyUnityLibrary/Coroutine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace MyUnityLibrary
+{
+    public class CoroutineAPI
+    {
+        /// <summary>
+        /// 定时器，在实际使用中通过调用 StartCoroutine(Wait(<秒数>)) 就可以了
+        /// </summary>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
+        public static IEnumerator Wait(float seconds)
+        {
+            yield return new WaitForSeconds(seconds);
+        }
+    }
+}
---
+++ MyUnityLibrary/Singleton.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace MyUnityLibrary
+{
+    public class CMonoBehaviour : MonoBehaviour
+    {
+        public virtual void Init()
+        {
+
+        }
+    }
+
+    public class SingletonMonoBehaviour<T> where T : CMonoBehaviour
+    {
+        protected static T instance;
+
+        /**
+          Returns the instance of this singleton.
+       */
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = (T)GameObject.FindObjectOfType(typeof(T));
+
+                    if (instance == null)
+                    {
+                        Debug.LogError("An instance of " + typeof(T) +
+                            " is needed in the scene, but there is none.");
+                    }
+
+                    instance.Init();
+                }
+                return instance;
+            }
+        }
+
+        public static void DestroyExtraObjects(GameObject obj)
+        {
+            if (obj != Instance.gameObject)
+            {
+                GameObject.Destroy(obj);
+            }
+        }
+
+        public static bool DestroyExtraObjects(MonoBehaviour scriptObj)
+        {
+            bool shouldDestroy = false;
+            if (scriptObj != Instance)
+            {
+                GameObject.Destroy(scriptObj.gameObject);
+                shouldDestroy = true;
+            }
+
+            return shouldDestroy;
+        }
+    }
+
+    public class Singleton<T> where T : class
+    {
+        protected static T instance;
+
+        /**
+          Returns the instance of this singleton.
+       */
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = System.Activator.CreateInstance(typeof(T)) as T;
+                    if (null == instance)
+                    {
+                        Debug.LogAssertion("Singleton object init failed: " + typeof(T).Name);
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public static void Destroy()
+        {
+            if (instance != null)
+            {
+                instance = null;
+            }
+        }
+    }
+
+    public class DontDestoryOnLoadBehaviour<T> where T : MonoBehaviour
+    {
+        public static void Check(T t, ref T instance)
+        {
+            Debug.LogFormat("this {0}, instance {1}", t, instance);
+
+            if (null == instance)
+            {
+                instance = t;
+            }
+            else if (t != instance)
+            {
+                GameObject.Destroy(t.gameObject);
+                return;
+            }
+            MonoBehaviour.DontDestroyOnLoad(t.gameObject);
+        }
+    }
+}
